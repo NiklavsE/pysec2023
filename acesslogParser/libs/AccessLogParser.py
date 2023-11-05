e80a4cd60c34b6934d5938ca1c6183e9b2af3acd
Niklavs <niklavs.emils.eglitis@gmail.com>
2023-11-05 22:09:35 +0200

    add log parsing task

---
+++ acesslogParser/libs/AccessLogParser.py
@@ -0,0 +1,33 @@
+import re
+from libs.utils import readFile
+import json
+from jsonpath_ng import jsonpath, parse
+import datetime
+
+class AccessLogParser(object):
+    def __init__(self):
+        pass
+
+    def info(self):
+        print("This is access log parser!")
+
+    log_file_path = 'access.log.example'
+
+    # Given a json path and value, return all the log records that match the value at the given path
+    def readFile(self, path, value):
+        matched_events = []
+
+        pathExpression = parse(path)
+
+        lines = readFile(self.log_file_path)
+        for log_dict in lines:
+            jsonData = json.loads(log_dict['json'])
+            
+            match = pathExpression.find(jsonData)
+            if (value == match[0].value):
+                matched_events.append({
+                    'timestamp': log_dict['timestamp'], 
+                    'json': log_dict['json']
+                })
+
+        return matched_events
---
+++ acesslogParser/main.py
@@ -0,0 +1,18 @@
+from libs.AccessLogParser import AccessLogParser
+from datetime import datetime
+
+##########################################################################
+# Small script that parses an undisclosed application's access log.
+# Example of the log records is provided in the access.log.example file.
+# Record filtering is done via JSON Path and the matched results (if any)
+# are compared against the provided value.
+# Required to run: jsonpath-ng==1.6.0
+##########################################################################
+
+AcessLogParser = AccessLogParser()
+AcessLogParser.info()
+# return all the log records that match the following value for request id
+events = AcessLogParser.readFile('$.request_id', 'ad9f0ce4296e562242cdb529f4044fbb')
+
+for event in events:
+    print(event)
